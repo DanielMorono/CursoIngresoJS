8ae584d46026ac7335e52b74852ac7a74f2e6671
Daniel <morno.daniel99@gmail.com>
2018-07-03 12:23:57 -0300

    Ejercicios

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,8 +2,24 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombre
+    //esto es un comentario de una linea
+    /*
+    Esto es un comentario de mas de una linea
+    */
+	var nombre;
     nombre=prompt("Ingrese su nombre","Nombre");
     alert("Su nombre es "+nombre);
+    /*
+    El + sirve para sumar o unir enlaces
+    Siempre se termina con ;
+    Los datos se guardan en variables(Siempre empiezan con una letra)
+    la variable siempre se escribe en minuscula salvo cuando hay mas de una palabra 
+    y se usa mayuscula en las siguiestes(Camel case)
+    = Operador de asignacion
+    una variable por defeto es undefined, si tuvo valor y despues se le saco usamos null
+    Variable number toma numeros, non, infinit
+    Variable string es texto, va entre comillas
+
+    */
 }
 
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,9 @@
 */
 function mostrar()
 {
-	
+	var dato
+	dato=prompt("Escriba el dato")
+	alert=
+	//hacer hasta el ejercicio 6
 }
 
