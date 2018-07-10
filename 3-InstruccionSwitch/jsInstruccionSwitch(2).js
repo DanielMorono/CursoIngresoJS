a6d7d13093d893406bcbc85621209e6a1f25c6f1
Daniel <morono.daniel99@gmail.com>
2018-07-10 18:59:35 -0300

    IntroduccionSwitch 1 y 2

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,72 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño = document.getElementById('mes').value;
+    var preInvierno = "Falta para el invierno";
+    var invierno = "Abrigate que hace frio";
+    var postInvierno = "Ya pasamos el frio, ahora calor!!!";
 
-alert (mesDelAño);
-
-
-
-
+    switch (mesDelAño) 
+    {
+        case "Enero" :
+        {
+            alert (preInvierno);
+            break;
+        }
+        case "Febrero" :
+        {
+            alert (preInvierno);
+            break;
+        }
+        case "Marzo" :
+        {
+            alert (preInvierno);
+            break;
+        }
+        case "Abril" :
+        {
+            alert (preInvierno);
+            break;
+        }
+        case "Mayo" :
+        {
+            alert (preInvierno);
+            break;
+        }
+        case "Junio" :
+        {
+            alert (preInvierno);
+            break;
+        }
+        case "Julio" :
+        {
+            alert (invierno);
+            break;
+        }
+        case "Agosto" :
+        {
+            alert (invierno);
+            break;
+        }
+        case "Septiembre" :
+        {
+            alert (postInvierno);
+            break;
+        }
+        case "Octubre" :
+        {
+            alert (postInvierno);
+            break;
+        }
+        case "Noviembre" :
+        {
+            alert (postInvierno);
+            break;
+        }
+        case "Diciembre" :
+        {
+            alert (postInvierno);
+            break;
+        }
+    }
 }//FIN DE LA FUNCIÓN
